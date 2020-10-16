68c761c733112ce6e49ab282786b587c91d3c23b
a-shriya <anegondi.shriya@gmail.com>
2020-10-16 15:53:06 +0530

    Added animator

---
+++ app/src/main/java/com/example/booksmanagement/recyclerView/CustomItemAnimator.java
@@ -0,0 +1,25 @@
+package com.example.booksmanagement.recyclerView;
+
+import android.view.animation.AnimationUtils;
+
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.booksmanagement.R;
+
+public class CustomItemAnimator extends DefaultItemAnimator
+{
+    @Override
+    public boolean animateRemove(RecyclerView.ViewHolder holder)
+    {
+        holder.itemView.setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.viewholder_remove_anim));
+        return super.animateRemove(holder);
+    }
+
+    @Override
+    public boolean animateAdd(RecyclerView.ViewHolder holder)
+    {
+        holder.itemView.setAnimation(AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.viewholder_add_anim));
+        return super.animateAdd(holder);
+    }
+}
